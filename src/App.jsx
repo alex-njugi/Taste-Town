232b32b215255ec3a7e25205b856b8b5867b58bc
richard <richard.wasonga@student.moringaschool.com>
2025-04-16 11:47:08 +0300

    updated error page and style

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Home from "./components/Home"
 import Menu from "./components/Menu"
+import Layout from './components/Layout'
+import Errorlink from './components/Errorlink'
 
 
 function App() {
     return (
     <Routes>
-      <Route exact path="/" element={<Home/>}/>
-      <Route path="/About" element={<About/>}/>
-      <Route path="/Contact" element={<Contact/>}/>
-      <Route path="/Menu" element={<Menu/>}/>
+      <Route path='/' element={<Layout/>}>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+        <Route path="/Menu" element={<Menu/>}/>
+      </Route>
+      <Route path='*' element={<Errorlink/>}/>
       
     </Routes>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import NavBar from './NavBar'
 import Loadpageimg from './Loadpageimg'
 
 const Home = () => {
   return (
     <div className='home-container'>
-        <NavBar />
         <Loadpageimg />
 
     </div>

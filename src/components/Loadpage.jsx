6573ce97b4dcbdf7183d75b13aaa1d6e740b65eb
richard <richard.wasonga@student.moringaschool.com>
2025-04-16 09:10:16 +0300

    corrected letter typo

--- src/components/Loadpage.jsx
+++ src/components/Loadpage.jsx
@@ -4,7 +4,7 @@ const Loadpage = () => {
   return (
     <div>
         <h1>Let's now shop<br></br> For daily food<br></br> & necessary.</h1>
-        <p>We are truseted grocery shop you can buy <br></br>your necesary products use your phone.s</p>
+        <p>We are truseted grocery shop you can buy <br></br>your necesary products use your phones</p>
         <button className='shop-now-btn'>Shop Now</button>
         <button className='download-btn'>Download for ios</button>
     </div>

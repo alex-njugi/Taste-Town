b0d45ea85dcaf168e8f459086c04476c12d82f59
Rodney <rodney.amani@student.moringaschool.com>
2025-04-22 10:41:05 +0300

    DeleteProduct

--- src/components/DeleteProducts.jsx
+++ src/components/DeleteProducts.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const DeleteProducts = () => {
   return (
-    <div>DeleteProducts</div>
+    <div className='delete-products'>
+      <h1>Delete Products</h1>
+      <p>Are you sure you want to delete this product?</p>
+      <button className='delete-btn'>Delete</button>
+      <button className='cancel-btn'>Cancel</button>
+      <p>Product deleted successfully!</p>
+      <p>Product not found.</p>
+      
+    </div>
   )
 }
 
